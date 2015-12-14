cdb78d2f1a4470f86589dbd9372a6f2eaebec4a6
Johannes Stein <johannesstein@freeze-dev.com>
2015-12-14 09:31:25 -0800

    First round of lint fixes

--- index.js
+++ index.js
@@ -3,43 +3,43 @@ var snakeCase = require('snake-case');
 var paramCase = require('param-case');
 
 var changeKeys = function changeKeys(transformer, obj) {
-    if (Array.isArray(obj)) {
-        var r = [];
-        for (var i = 0; i < obj.length; i++) {
-            r.push(changeKeys(transformer, obj[i]));
-        }
-        return r;
-    } else if (typeof obj === 'object') {
-        var objectKeys = Object.keys(obj);
-        return objectKeys.map(function keysMap(key) {
-            return transformer(key);
-        }).reduce(function keysReducer(object, changedKey, index) {
-            var objValue = obj[objectKeys[index]];
-            var transformedValue = changeKeys(transformer, objValue);
-            object[changedKey] = transformedValue;
-            return object;
+  if (Array.isArray(obj)) {
+    var r = [];
+    for (var i = 0; i < obj.length; i++) {
+        r.push(changeKeys(transformer, obj[i]));
+      }
+    return r;
+  } else if (typeof obj === 'object') {
+      var objectKeys = Object.keys(obj);
+      return objectKeys.map(function keysMap(key) {
+        return transformer(key);
+      }).reduce(function keysReducer(object, changedKey, index) {
+          var objValue = obj[objectKeys[index]];
+          var transformedValue = changeKeys(transformer, objValue);
+          object[changedKey] = transformedValue;
+          return object;
         }, {});
     }
     else if (typeof obj === 'string') {
-        return transformer(obj);
+      return transformer(obj);
     }
     else {
-        return obj;
+      return obj;
     }
 
 };
 
 var changeCaseObject = {};
 changeCaseObject.camel = changeCaseObject.camelCase = function camelCaseObject(obj) {
-    return changeKeys(camelCase, obj);
+  return changeKeys(camelCase, obj);
 };
 
 changeCaseObject.snake = changeCaseObject.snakeCase = function snakeCaseObject(obj) {
-    return changeKeys(snakeCase, obj);
+  return changeKeys(snakeCase, obj);
 };
 
 changeCaseObject.param = changeCaseObject.paramCase = function paramCaseObject(obj) {
-    return changeKeys(paramCase, obj);
+  return changeKeys(paramCase, obj);
 };
 
 module.exports = changeCaseObject;
--- test/index.js
+++ test/index.js
@@ -3,114 +3,114 @@ var expect = chai.expect;
 
 var changeCaseObject = require('../index');
 
-describe('change-case-object', function () {
-    it('camelCase', function () {
-        var initialObj = {
-            'hello_world': 'test',
-        };
+describe('change-case-object', function() {
+  it('camelCase', function() {
+    var initialObj = {
+        'hello_world': 'test',
+      };
 
-        var fixtureObj = {
-            'helloWorld': 'test',
-        };
-        expect(changeCaseObject.camelCase(initialObj)).to.deep.equal(fixtureObj);
-    });
-    it('camelCase (Array of Object)', function () {
-        var initialArrObj = [
+    var fixtureObj = {
+        'helloWorld': 'test',
+      };
+    expect(changeCaseObject.camelCase(initialObj)).to.deep.equal(fixtureObj);
+  });
+  it('camelCase (Array of Object)', function() {
+    var initialArrObj = [
             {'hello_world': 'test'},
             {is_Active: true},
             {isActive: true}
-        ];
+      ];
 
-        var fixtureArrObj = [
+    var fixtureArrObj = [
             {'helloWorld': 'test'},
             {isActive: true},
             {isActive: true}
-        ];
-        expect(changeCaseObject.camelCase(initialArrObj)).to.deep.equal(fixtureArrObj);
-    });
-    it('camelCase (Array of string)', function () {
-        var initialArrStr = ["is_active", "is_blocked"];
+      ];
+    expect(changeCaseObject.camelCase(initialArrObj)).to.deep.equal(fixtureArrObj);
+  });
+  it('camelCase (Array of string)', function() {
+    var initialArrStr = ['is_active', 'is_blocked'];
 
-        var fixtureArrStr = ["isActive", "isBlocked"];
-        expect(changeCaseObject.camelCase(initialArrStr)).to.deep.equal(fixtureArrStr);
-    });
+    var fixtureArrStr = ['isActive', 'isBlocked'];
+    expect(changeCaseObject.camelCase(initialArrStr)).to.deep.equal(fixtureArrStr);
+  });
 
 
-    it('camelCase (deep)', function () {
-        var initialObj = {
-            'hello_world': {
-                'hello_there': 'name',
-                'my_name': 'someone',
-            },
-        };
+  it('camelCase (deep)', function() {
+    var initialObj = {
+        'hello_world': {
+            'hello_there': 'name',
+            'my_name': 'someone',
+          },
+      };
 
-        var fixtureObj = {
-            'helloWorld': {
-                'helloThere': 'name',
-                'myName': 'someone',
-            },
-        };
+    var fixtureObj = {
+        'helloWorld': {
+            'helloThere': 'name',
+            'myName': 'someone',
+          },
+      };
 
-        expect(changeCaseObject.camelCase(initialObj)).to.deep.equal(fixtureObj);
-    });
+    expect(changeCaseObject.camelCase(initialObj)).to.deep.equal(fixtureObj);
+  });
 
-    it('paramCase', function () {
-        var initialObj = {
-            'helloWorld': 'test',
-        };
+  it('paramCase', function() {
+    var initialObj = {
+        'helloWorld': 'test',
+      };
 
-        var fixtureObj = {
-            'hello-world': 'test',
-        };
+    var fixtureObj = {
+        'hello-world': 'test',
+      };
 
-        expect(changeCaseObject.paramCase(initialObj)).to.deep.equal(fixtureObj);
-    });
+    expect(changeCaseObject.paramCase(initialObj)).to.deep.equal(fixtureObj);
+  });
 
-    it('paramCase (deep)', function () {
-        var initialObj = {
-            'helloWorld': {
-                'helloThere': 'name',
-                'myName': 'someone',
-            },
-        };
+  it('paramCase (deep)', function() {
+    var initialObj = {
+        'helloWorld': {
+            'helloThere': 'name',
+            'myName': 'someone',
+          },
+      };
 
-        var fixtureObj = {
-            'hello-world': {
-                'hello-there': 'name',
-                'my-name': 'someone',
-            },
-        };
+    var fixtureObj = {
+        'hello-world': {
+            'hello-there': 'name',
+            'my-name': 'someone',
+          },
+      };
 
-        expect(changeCaseObject.paramCase(initialObj)).to.deep.equal(fixtureObj);
-    });
+    expect(changeCaseObject.paramCase(initialObj)).to.deep.equal(fixtureObj);
+  });
 
-    it('snakeCase', function () {
-        var initialObj = {
-            'helloWorld': 'test',
-        };
+  it('snakeCase', function() {
+    var initialObj = {
+        'helloWorld': 'test',
+      };
 
-        var fixtureObj = {
-            'hello_world': 'test',
-        };
+    var fixtureObj = {
+        'hello_world': 'test',
+      };
 
-        expect(changeCaseObject.snakeCase(initialObj)).to.deep.equal(fixtureObj);
-    });
+    expect(changeCaseObject.snakeCase(initialObj)).to.deep.equal(fixtureObj);
+  });
 
-    it('snakeCase (deep)', function () {
-        var initialObj = {
-            'helloWorld': {
-                'helloThere': 'name',
-                'myName': 'someone',
-            },
-        };
+  it('snakeCase (deep)', function() {
+    var initialObj = {
+        'helloWorld': {
+            'helloThere': 'name',
+            'myName': 'someone',
+          },
+      };
 
-        var fixtureObj = {
-            'hello_world': {
-                'hello_there': 'name',
-                'my_name': 'someone',
-            },
-        };
+    var fixtureObj = {
+        'hello_world': {
+            'hello_there': 'name',
+            'my_name': 'someone',
+          },
+      };
 
-        expect(changeCaseObject.snakeCase(initialObj)).to.deep.equal(fixtureObj);
-    });
+    expect(changeCaseObject.snakeCase(initialObj)).to.deep.equal(fixtureObj);
+  });
 });
